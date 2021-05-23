3fd64ee21ff030e637ce9fd77ff90dfa7b923904
Oscar Bernal <oscarbernal93@gmail.com>
2021-05-22 19:48:43 -0500

    add the alian reboot and improve battery script

--- script/battery_check.sh
+++ script/battery_check.sh
@@ -2,14 +2,11 @@
 
 LEVEL=$(cat /sys/class/power_supply/BATC/charge_now)
 MAX_LEVEL=$(cat /sys/class/power_supply/BATC/charge_full)
+STATUS=$(cat /sys/class/power_supply/BATC/status)
 PERCENT=$(($LEVEL*100/$MAX_LEVEL))
 
-if [ $PERCENT -lt 20 ]; then
-  echo "Battery level: $PERCENT%"
-fi
-
-if [ $PERCENT -lt 10 ]; then
-  echo "Battery level: $PERCENT%"
-  echo "Turning off the system!"
-  $(sudo systemctl poweroff)
+if [ "Discharging" = "$STATUS" ]; then
+  if [ $PERCENT -lt 15 ]; then
+    $(sudo systemctl poweroff)
+  fi
 fi
