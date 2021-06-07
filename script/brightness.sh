c7e410669ae1fe4f85bf288cde591e55e424bf0a
Oscar Bernal <oscarbernal93@gmail.com>
2021-06-06 20:28:49 -0500

    Improve brightness control

--- script/brightness.sh
+++ script/brightness.sh
@@ -2,15 +2,23 @@
 
 LEVEL=$(cat /sys/class/backlight/intel_backlight/brightness)
 MAX_LEVEL=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-PERCENT=$(($LEVEL*100/$MAX_LEVEL))
+PERCENT=$((($LEVEL*100/$MAX_LEVEL)+1))
 ONE=$((($MAX_LEVEL)/100))
-NEXT=$(($1))
 
-if [ $NEXT -lt 100 ]; then
-  if [ $NEXT -gt 0 ]; then
+if [ -n "$1" ]; then
+  NEXT=$(($1))
+  if [ $NEXT -le 0 ]; then
+    echo "Value must be greater than zero."
+    exit
+  fi
+
+  if [ $NEXT -gt 100 ]; then
+    echo "Value must be lesser than one hundred."
+    exit
+  else
     LEVEL=$(($ONE * $NEXT))
     echo $LEVEL > /sys/class/backlight/intel_backlight/brightness
-    PERCENT=$(($LEVEL*100/$MAX_LEVEL))
+    PERCENT=$((($LEVEL*100/$MAX_LEVEL)+1))
   fi
 fi
 
