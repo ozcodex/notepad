2fbc1e65bc1b8991b1cf4dbd1c31979994464131
Oscar Bernal <39655326+ozcodex@users.noreply.github.com>
2022-01-13 19:46:45 -0500

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// Javascript being Javascript
+
+1 < 2 < 3 // true
+3 > 2 > 1 // false
+
+[] + [] // '' Empty string
+[] + {} // [object Object]
+{} + [] // 0
+{} + {} // NaN
+
+[] + 1 // "1"
+{} + 1 // 1 
+[] - 1 // -1
+
+'11' + 1 // 111
+'11' - 1 // 10
+
+typeof null === 'object' //true
+
+typeof NaN // Number
+NaN instanceof Number //false
+NaN == NaN // false
